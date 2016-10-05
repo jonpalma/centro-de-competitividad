d3de9202267cd1eeeb02a660da987d964fb892ea
PedroBorundaMixen <pedro.borunda@mixen.mx>
2016-10-05 15:28:30 -0600

    Cambio banner diseño cc-foro

--- cc-foro.php
+++ cc-foro.php
@@ -12,13 +12,12 @@
             <div class="carousel-inner" role="listbox">
                 <div class="item active">
                     <div class="img-container">
-                        <img class="banner-bg" src="img/cc-foro/banner/bg2.jpg" alt="Banner">
+                        <img src="img/cc-foro/banner/bg2.jpg" alt="Banner">
                     </div>
                     <div class="container">
                         <div class="title">
                             <p>
-                                TRABAJAMOS EN EQUIPO Y<br/>
-                                OBTENEMOS LOGROS EXTRAORDINARIOS.
+                                TRABAJAMOS EN EQUIPO Y OBTENEMOS LOGROS EXTRAORDINARIOS.
                             </p>
                         </div>
                         <div class="logo-img-container">
@@ -26,14 +25,16 @@
                         </div>
                     </div>
                 </div>
-                <div class="indicators">
-                    <!-- Indicators -->
-                    <ol class="carousel-indicators">
-                        <li data-target="#carousel" data-slide-to="0" class="active"></li>
-                        <!--<li data-target="#carousel" data-slide-to="1"></li>-->
-                    </ol>
-                </div>
             </div>
+            <!-- Controls -->
+            <a class="left carousel-control" href="#carousel-example-generic" role="button" data-slide="prev">
+                <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
+                <span class="sr-only">Previous</span>
+            </a>
+            <a class="right carousel-control" href="#carousel-example-generic" role="button" data-slide="next">
+                <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
+                <span class="sr-only">Next</span>
+            </a>
         </div>
     </section>
     <!--~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~-->
