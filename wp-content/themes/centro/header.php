03d338df57836ada82903c0f15fcfde27d61deae
PedroBorundaMixen <pedro.borunda@mixen.mx>
2016-12-01 17:34:25 -0700

    Analytics

--- wp-content/themes/centro/header.php
+++ wp-content/themes/centro/header.php
@@ -11,8 +11,22 @@
         <link rel="stylesheet" href="<?php echo get_stylesheet_uri(); ?>">
         <?php wp_head(); ?>
         <title>CChCP</title>
+        <script type="text/javascript">
+            setTimeout(function(){var a=document.createElement("script");
+                                  var b=document.getElementsByTagName("script")[0];
+                                  a.src=document.location.protocol+"//script.crazyegg.com/pages/scripts/0020/7404.js?"+Math.floor(new Date().getTime()/3600000);
+                                  a.async=true;a.type="text/javascript";b.parentNode.insertBefore(a,b)}, 1);
+        </script>
     </head>
     <body>
+        <script>
+            (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+                (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+                m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+                                    })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+            ga('create', 'UA-88357628-1', 'auto');
+            ga('send', 'pageview');
+        </script>
         <nav class="navbar navbar-default navbar-fixed-top">
             <div class="top-logo">
             </div>
@@ -42,8 +56,8 @@
                         <li><a class="vertical-align smoothScroll" href="<?php if($page != 'index') { echo 'index'; }?>#servicios">Servicios</a></li>
                         <li><a class="vertical-align smoothScroll" href="<?php if($page != 'index') { echo 'index'; }?>#blog">Blog</a></li>
                         <!--
-                        <li><a class="vertical-align smoothScroll" href="<?php if($page != 'index') { echo 'index'; }?>#">Preguntas Frecuentes</a></li>
-                        -->
+<li><a class="vertical-align smoothScroll" href="<?php if($page != 'index') { echo 'index'; }?>#">Preguntas Frecuentes</a></li>
+-->
                         <li><a class="vertical-align smoothScroll email-icon" href="<?php if($page != 'index') { echo 'index'; }?>#contacto">Contacto</a></li>
 
                     </ul>
